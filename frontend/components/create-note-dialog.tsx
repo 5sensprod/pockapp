5a125915f98947873ddf35fb1d839c6ccb27c0d2
chris <5sensprod@gmail.com>
2025-11-15 17:20:59 +0100

    build

--- frontend/components/create-note-dialog.tsx
+++ frontend/components/create-note-dialog.tsx
@@ -1,78 +1,78 @@
+// create-note-dialog.tsx
 import { usePocketBase } from '@/use-pocketbase'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useNavigate } from '@tanstack/react-router'
-import { type RefObject, forwardRef, useState } from 'react'
+import { type RefObject } from 'react'
 import {
-	Dialog,
-	DialogContent,
-	DialogHeader,
-	DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
-export const CreateNoteDialog = forwardRef<
-	HTMLDivElement,
-	{
-		inputTitleRef: RefObject<HTMLInputElement>
-		open: boolean
-		onOpenChange: (open: boolean) => void
-	}
->((props, _ref) => {
-	const queryClient = useQueryClient()
-	const pb = usePocketBase()
-	const navigate = useNavigate()
-	const createNotes = useMutation({
-		mutationFn(params: { title: string; content: string }) {
-			return pb.collection('notes').create(params)
-		},
-		onSuccess(data) {
-			queryClient.invalidateQueries({ queryKey: ['notes'] })
-			props.onOpenChange(false)
+type CreateNoteDialogProps = {
+  inputTitleRef: RefObject<HTMLInputElement>
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
 
-			navigate({
-				to: '/notes/$noteId',
-				params: {
-					noteId: data.id,
-				},
-			})
-		},
-	})
+export function CreateNoteDialog(props: CreateNoteDialogProps) {
+  const queryClient = useQueryClient()
+  const pb = usePocketBase()
+  const navigate = useNavigate()
 
-	return (
-		<Dialog open={props.open} onOpenChange={props.onOpenChange}>
-			<DialogContent className='sm:max-w-[425px]'>
-				<DialogHeader>
-					<DialogTitle>Create a new note</DialogTitle>
-				</DialogHeader>
-				<form
-					className='flex gap-2 pt-4'
-					onSubmit={(e) => {
-						e.preventDefault()
-						const title = new FormData(e.target as HTMLFormElement).get('title')
+  const createNotes = useMutation({
+    mutationFn(params: { title: string; content: string }) {
+      return pb.collection('notes').create(params)
+    },
+    onSuccess(data) {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+      props.onOpenChange(false)
 
-						if (!title) {
-							return
-						}
+      navigate({
+        to: '/notes/$noteId',
+        params: {
+          noteId: data.id,
+        },
+      })
+    },
+  })
 
-						createNotes.mutate({
-							content: `# Your new note`,
-							title: title.toString(),
-						})
-					}}
-				>
-					<Input
-						name='title'
-						type='text'
-						required
-						placeholder='Write a title'
-						className='flex-1'
-						ref={props.inputTitleRef}
-					/>
+  return (
+    <Dialog open={props.open} onOpenChange={props.onOpenChange}>
+      <DialogContent className='sm:max-w-[425px]'>
+        <DialogHeader>
+          <DialogTitle>Create a new note</DialogTitle>
+        </DialogHeader>
+        <form
+          className='flex gap-2 pt-4'
+          onSubmit={(e) => {
+            e.preventDefault()
+            const title = new FormData(e.target as HTMLFormElement).get('title')
 
-					<Button type='submit'>Create</Button>
-				</form>
-			</DialogContent>
-		</Dialog>
-	)
-})
+            if (!title) {
+              return
+            }
+
+            createNotes.mutate({
+              content: `# Your new note`,
+              title: title.toString(),
+            })
+          }}
+        >
+          <Input
+            name='title'
+            type='text'
+            required
+            placeholder='Write a title'
+            className='flex-1'
+            ref={props.inputTitleRef}
+          />
+          <Button type='submit'>Create</Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- frontend/routes/notes/index.tsx
+++ frontend/routes/notes/index.tsx
@@ -1,35 +1,40 @@
 import { CreateNoteDialog } from '@/components/create-note-dialog'
 import { NotesList } from '@/components/notes-list'
 import { createFileRoute } from '@tanstack/react-router'
-import { createRef } from 'react'
+import { useRef, useState } from 'react'
 
 export const Route = createFileRoute('/notes/')({
-	component() {
-		const dialogRef = createRef<HTMLDialogElement>()
-		const inputTitleRef = createRef<HTMLInputElement>()
+  component() {
+    const [open, setOpen] = useState(false)
+    const inputTitleRef = useRef<HTMLInputElement>(null)
 
-		return (
-			<div className='w-full grid grid-cols-5'>
-				<div className='col-span-1 flex flex-col gap-2 p-2'>
-					<button
-						className='btn btn-primary'
-						type='button'
-						onClick={() => {
-							if (dialogRef.current) {
-								dialogRef.current.showModal()
-								inputTitleRef.current?.focus()
-							}
-						}}
-					>
-						Create
-					</button>
-					<NotesList />
-				</div>
-				<div className='col-span-4'>
-					<p>Select a note or create a new one</p>
-				</div>
-				<CreateNoteDialog ref={dialogRef} inputTitleRef={inputTitleRef} />
-			</div>
-		)
-	},
+    return (
+      <div className='w-full grid grid-cols-5'>
+        <div className='col-span-1 flex flex-col gap-2 p-2'>
+          <button
+            className='btn btn-primary'
+            type='button'
+            onClick={() => {
+              setOpen(true)
+              // Laisser React ouvrir le Dialog puis focus
+              setTimeout(() => {
+                inputTitleRef.current?.focus()
+              }, 0)
+            }}
+          >
+            Create
+          </button>
+          <NotesList />
+        </div>
+        <div className='col-span-4'>
+          <p>Select a note or create a new one</p>
+        </div>
+        <CreateNoteDialog
+          open={open}
+          onOpenChange={setOpen}
+          inputTitleRef={inputTitleRef}
+        />
+      </div>
+    )
+  },
 })
