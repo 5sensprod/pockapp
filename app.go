1891143c5b4ccff6322514057f56f4861beb0bd9
chris <5sensprod@gmail.com>
2026-01-03 22:46:41 +0100

    chore: bump version to 1.0.3

--- app.go
+++ app.go
@@ -59,7 +59,7 @@ func (a *App) waitForPocketBase() {
 
 // GetAppVersion retourne la version de l'app
 func (a *App) GetAppVersion() string {
-	return "1.0.2"
+	return "1.0.3"
 }
 
 // OpenFileDialog ouvre un sélecteur de fichiers natif
